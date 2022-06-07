39d999aacc56f7bb3a25e3e36a19997138bec81b
mauricioft <mauricioft93@gmail.com>
2022-06-06 21:40:23 -0500

    Change export components syntax

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Layout
-import Spacer from './layout/Spacer'
+export { default as Spacer} from './layout/Spacer'
 
 // Atoms
-import { Button } from './atoms/Button'
-import { Icon } from './atoms/Icon'
-import { Heading } from './atoms/Heading'
-import { Input } from './atoms/Input'
-import { Picture } from './atoms/Picture'
+export { Button } from './atoms/Button'
+export { Icon } from './atoms/Icon'
+export { Heading } from './atoms/Heading'
+export { Input } from './atoms/Input'
+export { Picture } from './atoms/Picture'
 
 // Molecules
-import { ButtonIcon } from './molecules/ButtonIcon'
-import { Dropdown } from './molecules/Dropdown'
-
-export { Spacer, Button, Icon, Heading, Input, Picture, ButtonIcon, Dropdown }
+export { ButtonIcon } from './molecules/ButtonIcon'
+export { Dropdown } from './molecules/Dropdown'
